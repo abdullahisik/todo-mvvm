d5b230bfb1685a44c5a39ecabd4fa70d4db815d3
abdullahisik <romturkdevs@gmail.com>
2021-05-30 19:10:57 +0300

    Created UiTodoEntryActivity

---
+++ app/src/main/java/com/mitoz/todo/ui/UiTodoEntryActivity.kt
@@ -0,0 +1,13 @@
+package com.mitoz.todo.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.mitoz.todo.R
+
+class UiTodoEntryActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_ui_todo_entry)
+        
+    }
+}
