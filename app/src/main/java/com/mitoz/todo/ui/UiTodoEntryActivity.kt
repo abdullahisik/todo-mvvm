3fa986a67443bbd59fdbc56a471fc24ff09d537d
abdullahisik <romturkdevs@gmail.com>
2021-06-01 00:09:37 +0300

    update

--- app/src/main/java/com/mitoz/todo/ui/UiTodoEntryActivity.kt
+++ app/src/main/java/com/mitoz/todo/ui/UiTodoEntryActivity.kt
@@ -1,22 +1,39 @@
 package com.mitoz.todo.ui
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.core.widget.doAfterTextChanged
 import androidx.core.widget.doOnTextChanged
+import com.mitoz.todo.MainActivity
 import com.mitoz.todo.R
+import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.activity_ui_todo_entry.*
 
 class UiTodoEntryActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_ui_todo_entry)
+        supportActionBar?.apply {
+            title = "Olacak"
+            elevation = 15F
+
+
+        }
+        buttonToolbarEntry.setOnClickListener {
+            val inputTitle = textinpuTitle.editText?.text.toString()
+            val inputDesction = textinpuTitle.editText?.text.toString()
+
+            val intent = Intent(applicationContext,MainActivity::class.java)
+            startActivity(intent)
+
+
+
+        }
 
 
 
 
-        val inputTitle = textinpuTitle.editText?.text.toString()
-        val inputDesction = textinpuTitle.editText?.text.toString()
 
 
 
