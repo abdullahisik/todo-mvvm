60a0f8854c8878701916d86c079ed3c30d8fece3
abdullahisik <romturkdevs@gmail.com>
2021-05-31 23:14:16 +0300

    update

--- app/src/main/java/com/mitoz/todo/ui/UiTodoEntryActivity.kt
+++ app/src/main/java/com/mitoz/todo/ui/UiTodoEntryActivity.kt
@@ -2,12 +2,25 @@ package com.mitoz.todo.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.widget.doAfterTextChanged
+import androidx.core.widget.doOnTextChanged
 import com.mitoz.todo.R
+import kotlinx.android.synthetic.main.activity_ui_todo_entry.*
 
 class UiTodoEntryActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_ui_todo_entry)
+
+
+
+
+        val inputTitle = textinpuTitle.editText?.text.toString()
+        val inputDesction = textinpuTitle.editText?.text.toString()
+
+
+
+
 
     }
 }
