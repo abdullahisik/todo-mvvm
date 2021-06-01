756eddd07c51f39c141d89205c3a41c3e3492a72
abdullahisik <romturkdevs@gmail.com>
2021-06-01 10:34:48 +0300

    date and time picker setup

--- app/src/main/java/com/mitoz/todo/ui/UiTodoEntryActivity.kt
+++ app/src/main/java/com/mitoz/todo/ui/UiTodoEntryActivity.kt
@@ -1,10 +1,14 @@
 package com.mitoz.todo.ui
 
 import android.app.DatePickerDialog
+import android.app.TimePickerDialog
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.Editable
+import android.view.View
+import android.widget.TimePicker
+import android.widget.Toast
 import androidx.core.widget.doAfterTextChanged
 import androidx.core.widget.doOnTextChanged
 import com.mitoz.todo.MainActivity
@@ -28,18 +32,10 @@ class UiTodoEntryActivity : AppCompatActivity() {
 
         }
 
-        val c = Calendar.getInstance()
-        val year = c.get(Calendar.YEAR)
-        val month = c.get(Calendar.MONTH)
-        val day = c.get(Calendar.DAY_OF_MONTH)
+
         textinputSchedule.setOnClickListener {
 
-            val dpd = DatePickerDialog(this, DatePickerDialog.OnDateSetListener { view, year, monthOfYear, dayOfMonth ->
-                // Display Selected date in TextView
-                val date = "" + dayOfMonth.toString() + " " + month.toString() + ", " + year.toString()
-                textinputSchedule.editText?.text =  date.toEditable()
-            }, year, month, day)
-            dpd.show()
+
         }
 
 
@@ -64,5 +60,35 @@ class UiTodoEntryActivity : AppCompatActivity() {
 
 
 
+    }
+
+
+    fun onclickSheduleDate(view: View) {
+        println("olur gibi")
+        val c = Calendar.getInstance()
+        val year = c.get(Calendar.YEAR)
+        val month = c.get(Calendar.MONTH)
+        val day = c.get(Calendar.DAY_OF_MONTH)
+        val dpd = DatePickerDialog(this, DatePickerDialog.OnDateSetListener { view, year, monthOfYear, dayOfMonth ->
+            // Display Selected date in TextView
+            val date = "" + dayOfMonth.toString() + ", " + month.toString() + ", " + year.toString()
+            textinputSchedule.editText?.text =  date.toEditable()
+        }, year, month, day)
+        dpd.show()
+    }
+
+    fun onclickTime(view: View) {
+        val mTimePicker: TimePickerDialog
+        val mcurrentTime = Calendar.getInstance()
+        val hour = mcurrentTime.get(Calendar.HOUR_OF_DAY)
+        val minute = mcurrentTime.get(Calendar.MINUTE)
+
+        mTimePicker = TimePickerDialog(this, object : TimePickerDialog.OnTimeSetListener {
+            override fun onTimeSet(view: TimePicker?, hourOfDay: Int, minute: Int) {
+                val time = hourOfDay.toString()+":"+minute.toString()
+                textinputTime.editText?.text = time.toEditable()
+            }
+        }, hour, minute, false)
+mTimePicker.show()
     }
 }
