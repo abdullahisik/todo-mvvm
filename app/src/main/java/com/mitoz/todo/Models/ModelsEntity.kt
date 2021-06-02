b10f3ff948f8dfcc0fc837b63956ae0308452664
abdullahisik <romturkdevs@gmail.com>
2021-06-02 22:23:58 +0300

    Update ModelsEntity.kt

--- app/src/main/java/com/mitoz/todo/Models/ModelsEntity.kt
+++ app/src/main/java/com/mitoz/todo/Models/ModelsEntity.kt
@@ -10,9 +10,10 @@ data class ModelsEntity(
     @PrimaryKey(autoGenerate = true) var Id: Int,
     @ColumnInfo(name = "title") var title: String,
     @ColumnInfo(name = "description") var description: String,
-    @ColumnInfo(name = "schedule") val schedule: Int,
+    @ColumnInfo(name = "schedule") val scheduleFlag: Int,
     @ColumnInfo(name = "photograph") val photograph: String,
     @ColumnInfo(name = "expand") var expand: Int,
     @ColumnInfo(name = "doneornot") var doneornot: String
+
 
     )
